9dd1479688b1e6967bd27d8101cffa8f9606e56b
umartahir-99 <tahirumar835@gmail.com>
2024-12-21 20:34:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Write a program that accepts two numbers and prints the larger number using if conditions.
+
+var n1 = prompt("Enter a 1st number:");
+var n2 = prompt("Enter a 2nd number:");
+if(n1 > n2 ){
+  console.log("1st number is Larger")
+
+}
+else if( n1 < n2){
+  console.log("2nd number is Larger")
+
+}
